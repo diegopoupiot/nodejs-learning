0dd9341db1a6544ba4d3573cadaf1f35d8593aaf
Diego Poupiot <poupiot.diego@gmail.com>
2024-07-09 15:19:23 +0200

    express js & config

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const mod1 = require('./module1');
+require('babel-register')
+const express = require('express')
+const app = express()
 
-mod1.sayHello()
-mod1.sayGoodbye()
 
-console.log(mod1.helloworld)
+app.use((req, res, next) => {
+    console.log('Time: %d', Date.now())
+    console.log('URL : ' + req.url)
+    next()
+})
 
-console.log(mod1.helloSecondeManiere)
+app.get('/api', (req, res) => {
+    res.send('Root API')
+})
+
+app.get('/api/v1', (req, res) => {
+    res.send('API version 1')
+})
+
+app.get('/api/v1/books/:id', (req, res) => {
+    res.send(`Book ID: ${req.params.id}`)
+})
+app.get('/api/v1/books/:id/:chapter', (req, res) => {
+    res.send(`Book ID: ${req.params.id}, Chapter: ${req.params.chapter}`)
+})
+
+app.listen(8081, () => {
+    console.log('Server is running on http://localhost:8081')
+})
