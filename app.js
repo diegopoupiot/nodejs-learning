7b9917e6f9cedc58a54585424f1f5ba7274ae184
Diego Poupiot <poupiot.diego@gmail.com>
2024-07-10 11:16:44 +0200

    Proprification du code + config.json

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const config = require('./config')
 
 const members = [
     {
@@ -20,96 +21,108 @@ const members = [
     }
 ]
 
+let MembersRouter = express.Router()
+
 app.use(morgan('dev'))
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.get('/api/v1/members/:id', (req, res) => {
+MembersRouter.route('/:id')
 
-    let index = getIndex(req.params.id)
+    // Recupère un membre par son id
+    .get((req, res) => {
 
-    if (typeof(index) == 'string') {
-        res.json(error(index))
-    }
-    else {
-        res.json(success(members[index].name))
-    }
-})
+        let index = getIndex(req.params.id)
 
-app.put('/api/v1/members/:id', (req, res) => {
-    let index = getIndex(req.params.id)
-    let same = false
-
-    if (typeof(index) == 'string') {
-        res.json(error(index))
-    }
-    else {
-
-        members.forEach(member => {
-            if (member.name === req.body.name && member.id !== req.params.id) {
-                same = true
-                return
-            }
-        })
-    }
-
-    if (same) {
-        res.json(error('Member already exists'))
-    } else {
-        members[index].name = req.body.name // Update the name
-        res.json(success(members[index]))
-    }
-})
-
-app.delete('/api/v1/members/:id', (req, res) => {
-    let index = getIndex(req.params.id)
-
-    if (typeof(index) == 'string') {
-        res.json(error(index))
-    }
-    else {
-        members.splice(index, 1) // supprime l'élément à l'index
-        res.json(success(members))
-    }
-})
-
-app.get('/api/v1/members', (req, res) => {
-    if (req.query.max !== undefined && req.query.max > 0) {
-        res.json(success(members.slice(0, req.query.max)))
-    } else if (req.query.max !== undefined) {
-        res.json(error('Wrong max value'))
-    } else {
-        res.json(success(members))
-    }
-})
-
-app.post('/api/v1/members', (req, res) => {
-
-    let alreadyExists = false
-
-    members.forEach(member => {
-        if (member.name === req.body.name) {
-            alreadyExists = true
-            return res.json(error('Member already exists'))
+        if (typeof(index) == 'string') {
+            res.json(error(index))
+        }
+        else {
+            res.json(success(members[index].name))
         }
     })
 
-    if (alreadyExists) return
+    // Modifie un membre par son id
+    .put( (req, res) => {
+        let index = getIndex(req.params.id)
+        let same = false
 
-    if (req.body.name) {
-        let id = getLastId()
-        let newMember = {
-            id: id,
-            name: req.body.name
+        if (typeof(index) == 'string') {
+            res.json(error(index))
+        }
+        else {
+
+            members.forEach(member => {
+                if (member.name === req.body.name && member.id !== req.params.id) {
+                    same = true
+                }
+            })
         }
 
-        members.push(newMember)
-        res.json(success(newMember))
-    } else res.json(error('Wrong data'))
-})
+        if (same) {
+            res.json(error('Member already exists'))
+        } else {
+            members[index].name = req.body.name // Update the name
+            res.json(success(members[index]))
+        }
+    })
 
-app.listen(8081, () => {
-    console.log('Server is running on http://localhost:8081')
+    // Supprime un membre par son id
+    .delete((req, res) => {
+        let index = getIndex(req.params.id)
+
+        if (typeof(index) == 'string') {
+            res.json(error(index))
+        }
+        else {
+            members.splice(index, 1) // supprime l'élément à l'index
+            res.json(success(members))
+        }
+    })
+
+MembersRouter.route('/')
+
+    // Recupère tous les membres
+    .get( (req, res) => {
+        if (req.query.max !== undefined && req.query.max > 0) {
+            res.json(success(members.slice(0, req.query.max)))
+        } else if (req.query.max !== undefined) {
+            res.json(error('Wrong max value'))
+        } else {
+            res.json(success(members))
+        }
+    })
+
+    // Ajoute un membre
+    .post((req, res) => {
+
+        let alreadyExists = false
+
+        members.forEach(member => {
+            if (member.name === req.body.name) {
+                alreadyExists = true
+                return res.json(error('Member already exists'))
+            }
+        })
+
+        if (alreadyExists) return
+
+        if (req.body.name) {
+            let id = getLastId()
+            let newMember = {
+                id: id,
+                name: req.body.name
+            }
+
+            members.push(newMember)
+            res.json(success(newMember))
+        } else res.json(error('Wrong data'))
+    })
+
+app.use(config.rootAPI + 'members', MembersRouter) // On laisse l'url a MembersRouter
+
+app.listen(config.port, () => {
+    console.log('Server is running on http://localhost:' + config.port)
 })
 
 function getIndex(id) {
