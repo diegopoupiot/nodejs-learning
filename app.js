a5864509f347983eb34220ff7d089336e775fa41
Diego Poupiot <poupiot.diego@gmail.com>
2024-07-11 10:46:18 +0200

    bdd init & bdd requests

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const config = require('./config')
+const mysql = require("mysql2");
 
 const members = [
     {
@@ -21,117 +22,166 @@ const members = [
     }
 ]
 
-let MembersRouter = express.Router()
-
-app.use(morgan('dev'))
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-MembersRouter.route('/:id')
-
-    // Recupère un membre par son id
-    .get((req, res) => {
-
-        let index = getIndex(req.params.id)
-
-        if (typeof(index) == 'string') {
-            res.json(error(index))
-        }
-        else {
-            res.json(success(members[index].name))
-        }
-    })
-
-    // Modifie un membre par son id
-    .put( (req, res) => {
-        let index = getIndex(req.params.id)
-        let same = false
-
-        if (typeof(index) == 'string') {
-            res.json(error(index))
-        }
-        else {
-
-            members.forEach(member => {
-                if (member.name === req.body.name && member.id !== req.params.id) {
-                    same = true
-                }
-            })
-        }
-
-        if (same) {
-            res.json(error('Member already exists'))
-        } else {
-            members[index].name = req.body.name // Update the name
-            res.json(success(members[index]))
-        }
-    })
-
-    // Supprime un membre par son id
-    .delete((req, res) => {
-        let index = getIndex(req.params.id)
-
-        if (typeof(index) == 'string') {
-            res.json(error(index))
-        }
-        else {
-            members.splice(index, 1) // supprime l'élément à l'index
-            res.json(success(members))
-        }
-    })
-
-MembersRouter.route('/')
-
-    // Recupère tous les membres
-    .get( (req, res) => {
-        if (req.query.max !== undefined && req.query.max > 0) {
-            res.json(success(members.slice(0, req.query.max)))
-        } else if (req.query.max !== undefined) {
-            res.json(error('Wrong max value'))
-        } else {
-            res.json(success(members))
-        }
-    })
-
-    // Ajoute un membre
-    .post((req, res) => {
-
-        let alreadyExists = false
-
-        members.forEach(member => {
-            if (member.name === req.body.name) {
-                alreadyExists = true
-                return res.json(error('Member already exists'))
-            }
-        })
-
-        if (alreadyExists) return
-
-        if (req.body.name) {
-            let id = getLastId()
-            let newMember = {
-                id: id,
-                name: req.body.name
-            }
-
-            members.push(newMember)
-            res.json(success(newMember))
-        } else res.json(error('Wrong data'))
-    })
-
-app.use(config.rootAPI + 'members', MembersRouter) // On laisse l'url a MembersRouter
-
-app.listen(config.port, () => {
-    console.log('Server is running on http://localhost:' + config.port)
+const db = mysql.createConnection({
+    host: 'localhost',
+    database: 'nodejs',
+    user: 'user',
+    password: 'password',
+    ssl: {
+        rejectUnauthorized: false,
+    }
 })
 
-function getIndex(id) {
-    for (let i = 0; i < members.length; i++) {
-        if (members[i].id == id) return i
-    }
-    return 'Wrong id'
-}
+db.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err)
+    } else {
+        console.log('connected as id ' + db.threadId)
 
-function getLastId() {
-    return members[members.length - 1].id + 1
-}
+        let MembersRouter = express.Router()
+
+        app.use(morgan('dev'))
+        app.use(bodyParser.json()) // for parsing application/json
+        app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+        MembersRouter.route('/:id')
+
+            // Recupère un membre par son id
+            .get((req, res) => {
+
+                db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
+                    if (err) {
+                        console.error(err)
+                    } else if (result[0] === undefined) {
+                        res.json(error('Unknown id'))
+                    } else {
+                        res.json(success(result))
+                    }
+                })
+            })
+
+            // Modifie un membre par son id
+            .put((req, res) => {
+
+                if (req.body.name) {
+
+                    db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
+                        if (err) {
+                            res.json(error(err))
+                        } else {
+                            if (result[0] !== undefined) {
+                                db.query('SELECT * FROM members WHERE name = ? AND id != ?', [req.body.name, req.params.id], (err, result) => {
+                                    if (err) {
+                                        res.json(error(err))
+                                    } else {
+                                        if (result[0] !== undefined) {
+                                            res.json(error('Member already exists'))
+                                        } else {
+                                            db.query('UPDATE members SET name = ? WHERE id = ?', [req.body.name, req.params.id], (err, result) => {
+                                                if (err) {
+                                                    res.json(error(err))
+                                                } else {
+                                                    res.json(success(result))
+                                                }
+                                            })
+                                        }
+
+                                    }
+                                })
+                            } else {
+                                res.json(error('Unknown id'))
+                            }
+                        }
+                    })
+
+                }
+
+            })
+
+            // Supprime un membre par son id
+            .delete((req, res) => {
+                db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
+                    if (err) {
+                        res.json(error(err))
+                    } else if (result[0] !== undefined) {
+                        db.query('DELETE FROM members WHERE id = ?', [req.params.id], (err, result) => {
+                          if (err) {
+                              res.json(error(err))
+                          } else {
+                              res.json(success(result))
+                          }
+                        })
+                    } else {
+                        res.json(error('Wrong id'))
+                    }
+                })
+            })
+
+        MembersRouter.route('/')
+
+            // Recupère tous les membres
+            .get( (req, res) => {
+                if (req.query.max !== undefined && req.query.max > 0) {
+
+                    db.query('SELECT * FROM members LIMIT 0, ?', [req.query.max], (err, result) => {
+                        if (err) {
+                            console.error(err)
+                        } else {
+                            res.json(success(result))
+                        }
+                    })
+
+                } else if (req.query.max !== undefined) {
+                    res.json(error('Wrong max value'))
+                } else {
+
+                    db.query('SELECT * FROM members', (err, result) => {
+                        if (err) {
+                            console.error(err)
+                        } else {
+                            res.json(success(result))
+                        }
+                    })
+                }
+            })
+
+            // Ajoute un membre
+            .post((req, res) => {
+
+                let alreadyExists = false
+
+                db.query('SELECT * FROM members WHERE name = ?', [req.body.name], (err, result) => {
+                    if (err) {
+                        res.json(error(err))
+                    } else if (result[0] !== undefined) {
+                        alreadyExists = true
+                        return res.json(error('Member already exists'))
+                    } else {
+                        if (req.body.name) {
+                            db.query('INSERT INTO members(name) VALUES(?)', [req.body.name], (err, result) => {
+                                if (err) {
+                                    console.error(err)
+                                } else {
+                                    db.query('SELECT * FROM members WHERE name = ?', [req.body.name], (err, result) => {
+                                        if (err) {
+                                            res.json(error(err.message))
+                                        } else {
+                                            res.json(success(result))
+                                        }
+                                    })
+                                }
+                            })
+                        } else res.json(error('Wrong data'))
+                    }
+                })
+
+
+            })
+
+        app.use(config.rootAPI + 'members', MembersRouter) // On laisse l'url a MembersRouter
+
+        app.listen(config.port, () => {
+            console.log('Server is running on http://localhost:' + config.port)
+        })
+    }
+})
